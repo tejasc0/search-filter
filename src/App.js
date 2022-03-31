0ffa2a8f1f481c4fe6ff605326258497298f1a36
tejasc0 <tejasc888@gmail.com>
2022-03-31 13:49:31 +0000

    added users

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Users } from './users';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="max-w-max mx-auto">
+        <input type="text" placeholder="Search..." className="border-2 rounded-md p-3 border-gray-400 mt-12 focus:placeholder:text-zinc-900" />
+      </div>
+
+      <ul className="list">
+        {Users.map((user) => (
+          <li className="listItem">{user.first_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
