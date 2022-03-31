2fba3719120252727159f963938a7063defcf0f8
tejasc0 <tejasc888@gmail.com>
2022-03-31 14:19:46 +0000

    upgraded to react 18

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import { Users } from './users';
+import { useState } from 'react';
 
 function App() {
+
+  const [query, setQuery] = useState([]);
+  console.log(query);
+
   return (
     <div className="App">
       <div className="max-w-max mx-auto">
-        <input type="text" placeholder="Search..." className="border-2 rounded-md p-3 border-gray-400 mt-12 focus:placeholder:text-zinc-900" />
+        <input type="text" placeholder="Search..." onChange={(e) => setQuery(e.target.value)} className="border-2 rounded-md p-3 border-gray-400 mt-12 focus:placeholder:text-zinc-900" />
       </div>
 
       <ul className="list">
         {Users.map((user) => (
-          <li className="listItem">{user.first_name}</li>
+          <li className="listItem" key={user.id}>{user.first_name}</li>
         ))}
       </ul>
     </div>
